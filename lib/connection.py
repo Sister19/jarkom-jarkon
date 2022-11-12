6ca83aa17720e505893d0254cf1700b8fc4e759d
roastland <tfpruslanali@gmail.com>
2022-11-13 01:47:25 +0700

    test connection

--- lib/connection.py
+++ lib/connection.py
@@ -1,19 +1,25 @@
-import socket
+from socket import socket, AF_INET, SOCK_DGRAM, SOL_SOCKET, SO_BROADCAST
 from .segment import Segment
 
 class Connection:
     def __init__(self, ip : str, port : int):
         # Init UDP socket
-        pass
+        self.ip = ip
+        self.port = port
+        self.socket = socket(AF_INET, SOCK_DGRAM)
+        self.socket.bind((ip, port))
+        self.socket.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
 
-    def send_data(self, msg : Segment, dest : ("ip", "port")):
+    def send_data(self, msg : Segment,  dest: tuple[str, int]):
         # Send single segment into destination
-        pass
+        self.socket.sendto(msg, dest)
 
     def listen_single_segment(self) -> Segment:
+        response, address = self.socket.recvfrom(32768)
+        return response, address
         # Listen single UDP datagram within timeout and convert into segment
-        pass
 
     def close_socket(self):
         # Release UDP socket
-        pass
+        self.socket.close()
+
