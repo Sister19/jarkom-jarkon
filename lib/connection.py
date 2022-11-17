4073132855c9c7b6a891d96ccf2b7f76496e355c
roastland <tfpruslanali@gmail.com>
2022-11-17 19:05:42 +0700

    update connection and segment

--- lib/connection.py
+++ lib/connection.py
@@ -2,7 +2,7 @@ from socket import socket, AF_INET, SOCK_DGRAM, SOL_SOCKET, SO_BROADCAST
 from .segment import Segment
 
 class Connection:
-    def __init__(self, ip : str, port : int):
+    def __init__(self, ip, port):
         # Init UDP socket
         self.ip = ip
         self.port = port
@@ -10,16 +10,22 @@ class Connection:
         self.socket.bind((ip, port))
         self.socket.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
 
-    def send_data(self, msg : Segment,  dest: tuple[str, int]):
+    def send_data(self, msg : Segment, dest):
         # Send single segment into destination
         self.socket.sendto(msg, dest)
 
     def listen_single_segment(self) -> Segment:
-        response, address = self.socket.recvfrom(32768)
-        return response, address
         # Listen single UDP datagram within timeout and convert into segment
+        response, address = self.socket.recvfrom(32768)
+        data = Segment()
+        data.set_from_bytes(response)
+        return data, address, data.valid_checksum()
 
     def close_socket(self):
         # Release UDP socket
         self.socket.close()
 
+    def set_listen_timeout(self, timeout : float):
+        # Set connection timeout
+        self.socket.settimeout(timeout)
+        
--- lib/segment.py
+++ lib/segment.py
@@ -10,65 +10,72 @@ class SegmentFlag:
         self.syn = bool(flag & SYN_FLAG)
         self.ack = bool(flag & ACK_FLAG)
         self.fin = bool(flag & FIN_FLAG)
-        self.newBytes = 0b00000000
+        self.new_bytes = 0b00000000
         
     def get_flag_bytes(self) -> bytes:
-        
-        if self.syn:
-            self.newBytes |= SYN_FLAG
-        if self.ack:
-            self.newBytes |= ACK_FLAG
-        if self.fin:
-            self.newBytes |= FIN_FLAG
-        return struct.pack("B", self.newBytes)
-            
         # Convert this object to flag in byte form
+        if self.syn:
+            self.new_bytes |= SYN_FLAG
+        if self.ack:
+            self.new_bytes |= ACK_FLAG
+        if self.fin:
+            self.new_bytes |= FIN_FLAG
+        return struct.pack("B", self.new_bytes)
+
+    # to check the flag
+    def is_syn(self):
+        return bool(self.new_bytes & self.syn)
+
+    def is_ack(self):
+        return bool(self.new_bytes & self.ack)
+
+    def is_fin(self):
+        return bool(self.new_bytes & self.fin)
+
         
 class Segment:
     # -- Internal Function --
     def __init__(self):
-        self.sequence = 0
-        self.ackNum = 0
+        self.seq_num = 0
+        self.ack_num = 0
         self.flag = SegmentFlag(0b00000000)
-        self.chksum = 0
+        self.checksum = 0
         self.data = b""
         # Initalize segment
 
     def __str__(self):
         # Optional, override this method for easier print(segmentA)
         output = ""
-        output += f"{'Sequence number':24} | {self.sequence}\n"
-        output += f"{'Acknowledgement number':24} | {self.ackNum}\n"
-        output += f"{'Flags':24} | (SYN {self.flag.syn}) (ACK {self.flag.ack}) (FIN {self.flag.fin})\n"
-        output += f"{'Checksum':24} | {hex(self.chksum)}\n"
-        output += f"{'Valid checksum':24} | {self.valid_checksum()}\n"
-        output += f"{'Data payload':24} | {len(self.data)} Bytes"
+        output += f"Sequence number: {self.seq_num} |   Acknowledgement number: {self.ack_num}\n"
+        output += f"Flags: (SYN {self.flag.syn}) (ACK {self.flag.ack}) (FIN {self.flag.fin})\n"
+        output += f"Checksum: {hex(self.checksum)}  |   Valid checksum: {self.valid_checksum()}\n"
+        output += f"Data payload: {len(self.data)} bytes"
         return output
 
     def __calculate_checksum(self) -> int:
         # Calculate checksum here, return checksum result
         checksum = 0x0000
         x = 0xFFFF
-
-        checksum = (checksum + self.sequence) & x
-        checksum = (checksum + self.ackNum) & x
-        char_flag =  struct.unpack("B", self.flag.get_flag_bytes())[0]
+        checksum = (checksum + self.seq_num) & x
+        checksum = (checksum + self.ack_num) & x
+        char_flag = struct.unpack("B", self.flag.get_flag_bytes())[0]
         checksum = (checksum + char_flag) & x
-        checksum = (checksum + self.chksum) & x
+        checksum = (checksum + self.checksum) & x
         for i in range(0, len(self.data), 2):
-            buffer         = self.data[i:i+2]
+            buffer = self.data[i:i+2]
             if len(buffer) == 1:
                 buffer += struct.pack("x")
-            chunk         = struct.unpack("H", buffer)[0]
+            chunk = struct.unpack("H", buffer)[0]
             checksum = (checksum + chunk) & x
         checksum = x - checksum
         return checksum
 
-
-    # -- Setter --
-    def set_header(self, header : dict):
-        self.sequence = header["sequence"]
-        self.ackNum = header["ack"]
+    # -- Setter --        
+    def set_seq_num(self, seq_num : int):
+        self.seq_num = seq_num
+        
+    def set_ack_num(self, ack_num : int):
+        self.ack_num = ack_num
 
     def set_payload(self, payload : bytes):
         self.data = payload
@@ -77,40 +84,41 @@ class Segment:
         x = 0b00000000
         for i in range(len(flag_list)):
             x |= flag_list[i]
-        self.flag     = SegmentFlag(x)
-
+        self.flag = SegmentFlag(x)
 
     # -- Getter --
     def get_flag(self) -> SegmentFlag:
         return self.flag
 
-    def get_header(self) -> dict:
-        return {"sequence" : self.sequence, "ack" : self.ackNum}
+    def get_seq_num(self) -> int:
+        return self.seq_num
+    
+    def get_ack_num(self) -> int:
+        return self.ack_num
 
     def get_payload(self) -> bytes:
         return self.data
         
-
-
     # -- Marshalling --
     def set_from_bytes(self, src : bytes):
-        self.sequence = struct.unpack("I", src[0:4])[0] # 0, 1, 2, 3
-        self.ackNum = struct.unpack("I", src[4:8])[0] # 4, 5, 6, 7
-        self.flag = SegmentFlag(struct.unpack("B", src[8:9])[0]) # 8
-        self.chksum = struct.unpack("H", src[10:12])[0] # 10, 11
-        self.data = src[12:] # 12, dst
         # From pure bytes, unpack() and set into python variable
+        self.seq_num = struct.unpack("I", src[0:4])[0] # 0, 1, 2, 3
+        self.ack_num = struct.unpack("I", src[4:8])[0] # 4, 5, 6, 7
+        self.flag = SegmentFlag(struct.unpack("B", src[8:9])[0]) # 8
+        self.checksum = struct.unpack("H", src[10:12])[0] # 10, 11
+        self.data = src[12:] # 12, dst
 
     def get_bytes(self) -> bytes:
-        result = b""
-        result += struct.pack("I", self.sequence)
-        result += struct.pack("I", self.sequence)
-        result += struct.pack("B", self.flag.get_flag_bytes())
-        result += struct.pack("x")
-        result += struct.pack("H", self.chksum())
-        return result
         # Convert this object to pure bytes
-
+        result = b""
+        result += struct.pack("I", self.seq_num)
+        result += struct.pack("I", self.ack_num)
+        result += self.flag.get_flag_bytes()
+        result += struct.pack("x")
+        self.checksum = self.__calculate_checksum()
+        result += struct.pack("H", self.checksum)
+        result += self.data
+        return result
 
     # -- Checksum --
     def valid_checksum(self) -> bool:
